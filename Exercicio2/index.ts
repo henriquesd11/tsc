d4c6a3626cfb449fbbd3c4a4d7e3e7133364309a
henrique <luiz.isbaes@feltracoepertusatti.com.br>
2025-03-13 18:45:38 -0300

    Exercicio 2, convertendo variavel numero annotacion por variavel convertida string inferencia

---
+++ Exercicio2/index.ts
@@ -0,0 +1,8 @@
+const numero1: number = 10;
+let convertNumber1ToString = numero1.toString();
+
+// using concatenation
+console.log("The number is " + convertNumber1ToString);
+
+// using template string
+console.log(`The number is ${convertNumber1ToString}`);
